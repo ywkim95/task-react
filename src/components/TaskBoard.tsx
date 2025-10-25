5b968248bea778a03be48ceed7f25dd8d895d2cd
KimYoungWoo <rladuddn3924@gmail.com>
2025-10-25 13:49:21 +0900

    TODO 11: 로직 개선

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -7,6 +7,7 @@ import {
 } from "../api/mock";
 import "../styles/taskBoard.css";
 import { calcProgress } from "../utils/progress";
+import _ from "lodash";
 
 export type TaskBoardProps = {
   projectId?: string;
@@ -48,6 +49,13 @@ export function TaskBoard({ projectId }: TaskBoardProps) {
     if (!projectId) return;
     // TODO 11: (로직개선) title 을 trim 하고, 빈 문자열이면 생성하지 않도록 하세요.
     // 추가 요구: 같은 제목의 task 가 이미 있는 경우 생성하지 않도록 방지하세요. (lodash 사용 가능)
+    if (!title.trim()) return;
+
+    const isExist = _.find(tasks, function (task) {
+      return task.title === title;
+    });
+    if (isExist) return;
+
     await createTask(projectId, title);
     setTitle("");
     refresh();
